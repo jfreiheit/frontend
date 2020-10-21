ef58252620b0f67f1ef2f0e2437175e33a3aeabe
freiheit <freiheit@htw-berlin.de>
2020-10-21 16:37:12 +0200

    einfuehrung

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListeComponent } from './liste/liste.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AboutComponent } from './about/about.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListeComponent,
+    DashboardComponent,
+    AboutComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/liste/liste.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'htw-liste',
+  templateUrl: './liste.component.html',
+  styleUrls: ['./liste.component.css']
+})
+export class ListeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
